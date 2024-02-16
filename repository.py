66ca0ca64ec3d9be179be851892c476f678bc27e
IlyaZhGIT <ilyazh0709@gmail.com>
2024-02-16 18:59:01 +0300

    first commit

---
+++ repository.py
@@ -0,0 +1,26 @@
+from sqlalchemy import select
+from database import new_sessions, TaskOrm
+from schemas import Task, TaskAdd
+
+
+class TaskRepository():
+    @classmethod
+    async def add_one(cls, data: TaskAdd) -> int:
+        async with new_sessions() as session:
+            task_dict = data.model_dump()
+
+            task = TaskOrm(**task_dict)
+            session.add(task)
+            await session.flush()
+            await session.commit()
+            return task.id
+    
+    @classmethod
+    async def find_all(cls) -> list[Task]:
+        async with new_sessions() as session:
+            query = select(TaskOrm)
+            result = await session.execute(query)
+            task_models = result.scalars().all()
+            task_shcemas = [Task.model_validate(task_models) for task in task_models]
+            return task_models
+
